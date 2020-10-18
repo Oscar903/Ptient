a5bc6dbba3af4a559a7d18a34200c914d54451e2
Oscar903 <72770818+Oscar903@users.noreply.github.com>
2020-10-18 15:41:25 -0500

    Add files via upload

--- src/components/login/login.component.ts
+++ src/components/login/login.component.ts
@@ -1,23 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
-
+import { UserService } from 'src/app/services/user.service';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  providers: [UserService]
 })
 export class LoginComponent implements OnInit {
+  public user: User;
+  constructor(private _userService: UserService, private router: Router) {
 
-  constructor(private router: Router) { }
+  }
 
   ngOnInit(): void {
   }
-  public btnLogin(){
-    
-    this.router.navigate(['/RegisterPatient']);  
- 
-  }
 
+  public btnLogin() {
+        this.router.navigate(['/records']);
+  }
 
 }
